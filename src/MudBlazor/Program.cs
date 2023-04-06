a5cbd042f4dcb0296a39f34240930474b9c157b0
Alessandro Federici <afederici75@gmail.com>
2023-04-06 11:46:42 -0400

    temp

--- src/MudBlazor/Program.cs
+++ src/MudBlazor/Program.cs
@@ -1,11 +1,17 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+using MyApplication.Data;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
+
 // Add services to the container.
-builder.Services.AddChatBot(builder.Configuration);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddMudServices();
 
 var app = builder.Build();
