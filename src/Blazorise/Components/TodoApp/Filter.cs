543270ace8236761828fa97e18b53bcf563edbc0
Alessandro Federici <afederici75@gmail.com>
2023-04-06 20:24:49 -0400

    Added Blazorize demo

---
+++ src/Blazorise/Components/TodoApp/Filter.cs
@@ -0,0 +1,8 @@
+﻿namespace MyNewBlazoriseApp.Components.TodoApp;
+
+public enum Filter
+{
+    All,
+    Active,
+    Completed,
+}
---
+++ src/Blazorise/Components/TodoApp/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace MyNewBlazoriseApp.Components.TodoApp;
+
+public class Todo
+{
+    public bool Completed { get; set; }
+
+    public string? Description { get; set; }
+}
