c0e201bce4b0a718c42b083c934a69ba580604c6
Alessandro Federici <afederici75@gmail.com>
2023-04-08 15:54:48 -0400

    Blazorise broke :(

--- src/BlazoriseChatApp/Components/TodoApp/Filter.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MyNewBlazoriseApp.Components.TodoApp;
-
-public enum Filter
-{
-    All,
-    Active,
-    Completed,
-}
--- src/BlazoriseChatApp/Components/TodoApp/Todo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MyNewBlazoriseApp.Components.TodoApp;
-
-public class Todo
-{
-    public bool Completed { get; set; }
-
-    public string? Description { get; set; }
-}
